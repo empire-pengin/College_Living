a4e9fc39244816ce24281205be4e025fe5d69c2b
barchi118 <rx0.unicorngundam.0118@gmail.com>
2020-11-22 11:54:04 +0900

    college3.jpgの修正

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,24 @@
   @endcomponent
   </div>
   <div class="container-fluid  flex-grow-1 bd-highlight">
+  <!-- ヘッダー -->
+  <div class="row">
+  ss
+  </div>
+  <!-- 体験談表示 -->
+  <div class="row  bg-primary">
+  @for($i=0;$i<=2;$i++)
+  <div class="col-3 shadow">
+    <img  src="{{ asset('img/college/'. $experience[$i]->image )}}"
+      class="img-fluid" alt="" style="">
+  </div>
+  @endfor
+  </div>
+  <!-- 物件表示 -->
+  <div class="row">
+  ss
+  </div>
+  <!-- フッター -->
   <div class="row">
   @component('components.footer')
   @endcomponent
