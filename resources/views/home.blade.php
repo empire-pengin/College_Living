0870467fa8921574cc1052f8e485c993ff1e76b6
itabashi <yukiitabashijp@gmail.com>
2020-12-06 17:35:54 +0900

    12/06　17：35  home.blade.php レスポンシブ修正

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,15 @@
     <div class="row  "
     style="background-image: url('{{ asset('img/header.jpg')}}'); background-size: cover; " >
     <!-- ヘッダー文章 -->
-    <div class="col-lg-4 offset-8">
+    <div class="col-lg-4 offset-8 d-none  d-lg-block">
+      <div class="mt-5">
+        <h1>College</h1>
+        <h1>　Living</h1>
+        <p>先輩の一人暮らしの体験談を見て、よりよい大学生活を送るためのお部屋探しサイト</p>
+      </div>
+    </div>
+    <!-- ヘッダー文章スマホ -->
+    <div class="col-6 d-sm-block d-lg-none">
       <div class="mt-5">
         <h1>College</h1>
         <h1>　Living</h1>
@@ -80,7 +88,7 @@
     </a>
   </div>
   <!-- スマホ用 -->
-  <div class="col-md-10 bg-white pt-2 px-2 text-dark d-sm-block d-lg-none " >
+  <div class="col-md-10 bg-white pt-2 px-2 text-dark d-sm-block d-lg-none center-block" >
     <a href="" class="justify-content-start d-flex">
       <img  src="{{ asset('img/college/'. $experience[$i]->image )}}"
       class=" border-bottom" alt=""style="width:100px;" >
@@ -115,7 +123,7 @@
     </div>
   </div>
   @for($i=1;$i<=4;$i++)
-  <!-- if奇数だったら画像左 文章は右 -->
+  <!-- if奇数だったら画像左 文章は右 --
   <!-- それ以外だったら画像右 文章は左 -->
   <div class="col-lg-4 col-5 my-4 mx-5   p-0 shadow">
     <a href="{{ asset('items/'.$i )}}" style="text-decoration:none;">
