968a55eb4aa14bdbeb4ceef0e46c5bff7ecf87c1
itabashi <yukiitabashijp@gmail.com>
2020-12-07 21:14:50 +0900

    コンフリクトなし

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,9 +9,9 @@
 </div>
 </div>
 <div class="d-flex bd-highlight pr-0  " >
-  <!-- フッター -->
-  <div class="col-12 sticky-top  p-0" style="">
-    @component('components.nav')
+  <!-- サイドバー -->
+  <div class="  bd-highlight d-none d-md-block">
+    @component('components.sidebar')
     @endcomponent
   </div>
 
@@ -20,24 +20,7 @@
     <div class="row  d-none d-md-block"
     style="background-image: url('{{ asset('img/header.jpg')}}'); background-size: cover; " >
     <!-- ヘッダー文章 -->
-<<<<<<< HEAD
-    <div class="col-lg-4 offset-8 d-none  d-lg-block">
-=======
-    <div class="col-lg-4 offset-8 d-none d-lg-block">
->>>>>>> b9b77581c2dc127e306a210d77da0d736fa866b2
-      <div class="mt-5">
-        <h1>College</h1>
-        <h1>　Living</h1>
-        <p>先輩の一人暮らしの体験談を見て、よりよい大学生活を送るためのお部屋探しサイト</p>
-      </div>
-    </div>
-<<<<<<< HEAD
-    <!-- ヘッダー文章スマホ -->
-    <div class="col-6 d-sm-block d-lg-none">
-=======
-    <!-- レスポンシブヘッダー文書 -->
-    <div class="col-12 d-block d-lg-none">
->>>>>>> b9b77581c2dc127e306a210d77da0d736fa866b2
+    <div class="col-lg-4 offset-8">
       <div class="mt-5">
         <h1>College</h1>
         <h1>　Living</h1>
@@ -115,7 +98,6 @@
       </div>
     </a>
   </div>
-<<<<<<< HEAD
   <!-- 体験談スマホ用 -->
   <div class="col-sm-10 col-md-10 mt-2 bg-white pt-2 px-2 text-dark d-sm-block d-lg-none " >
     <a href="" class="justify-content-start d-flex text-dark" style="text-decoration:none;">
@@ -123,11 +105,6 @@
       class=" border-bottom" alt=""style="width:200px;" >
       <div class="">
       <h6 class="mb-0 border-bottom border-danger pb-2 pl-1">
-=======
-  <!-- 体験談レスポンシブ用 -->
-  <div class="col-6 bg-white pt-2 px-2 text-dark d-block d-lg-none" >
-    <h6 class="mb-0 border-bottom border-danger pb-2 pl-1">
->>>>>>> b9b77581c2dc127e306a210d77da0d736fa866b2
       <strong>{{$experience[$i]->college_name}}</strong>
     </h6>
     <div class="" style="font-size:12px;">
@@ -150,46 +127,7 @@
 </div>
 
 <!-- 物件表示 -->
-<div class=" d-none d-lg-block">
-  <div class="row  d-flex justify-content-center">
-    <div class="col-12 mt-5 ">
-      <div class="center-block">
-        <h1 style="color:#FF4500;" class="text-center">Rent</h1>
-        <p style=" " class="text-center">山手線沿線の物件を確認できます！</p>
-      </div>
-    </div>
-    @for($i=1;$i<=4;$i++)
-    <div class="col-lg-4 col-8 my-4 mx-5   p-0 shadow ">
-      <a href="{{ asset('items/'.$i )}}" style="text-decoration:none;">
-        <img src="{{asset('img/item/naisou/'.$item[$i]->item_image1)}}"
-        class="img-fluid"  alt="">
-        <div class="bg-white text-dark pt-2 px-2 " style="">
-          <h6 class="mb-0 border-bottom border-danger pb-2 pl-1">
-            <strong>{{$item[$i]->name}}</strong>
-          </h6>
-          <div style="font-size:12px;">
-            <ul style="list-style:none; " class="pl-1 py-2">
-              <li>
-                {{'家賃'. number_format($item[$i]->rent).'円'}}/{{'山手線「'.$item[$i]->station.'」'}}/{{'築'.$item[$i]->age.'年'}}
-                /{{'間取り'.$item[$i]->construction}}
-              </li>
-            </ul>
-          </div>
-        </div>
-      </a>
-    </div>
-    @endfor
-    <div class="col-12">
-      <div class="center-block d-flex justify-content-center">
-        <a href="{{ asset('items')}}"
-        class="btn btn-outline-danger btn-lg my-4 " tabindex="-1" role="button" aria-disabled="true">
-        Click More
-      </a>
-    </div>
-  </div>
-</div>
-<!-- 物件レスポンシブ用 -->
-<div class="row d-block d-lg-none">
+<div class="row  d-flex justify-content-center">
   <div class="col-12 mt-5 ">
     <div class="center-block">
       <h1 style="color:#FF4500;" class="text-center">Rent</h1>
@@ -197,7 +135,6 @@
     </div>
   </div>
   @for($i=1;$i<=4;$i++)
-<<<<<<< HEAD
   <!-- 物件のパソコン表示 -->
   <div class="col-lg-4 my-4 mx-5 d-none  d-lg-block   p-0 shadow">
     <a href="{{ asset('items/'.$i )}}" style="text-decoration:none;">
@@ -226,16 +163,6 @@
       class="" style="width:200px;"  alt="">
       <div class="text-dark">
       <h6 class="mb-0  border-bottom border-danger pb-2 ">
-=======
-  <div class="col-6 p-0 shadow ">
-    <a href="{{ asset('items/'.$i )}}" style="text-decoration:none;">
-      <img src="{{asset('img/item/naisou/'.$item[$i]->item_image1)}}"
-      class="img-fluid"  alt="">
-    </a>
-  </div>
-  <div class="col-6 bg-white text-dark pt-2 px-2 " style="">
-    <h6 class="mb-0 border-bottom border-danger pb-2 pl-1">
->>>>>>> b9b77581c2dc127e306a210d77da0d736fa866b2
       <strong>{{$item[$i]->name}}</strong>
     </h6>
     <div style="font-size:12px;">
